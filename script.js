17b32b0ebb41ac59c55d36112130b44c391c9dcd
Dylan76324 <popatdylan@gmail.com>
2026-06-09 14:45:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Football Manager script loaded");
+function startGame() {
+    const team = document.getElementById("teamSelect").value;
+
+    if (team === "") {
+        document.getElementById("output").innerText = "Please choose a team first.";
+        return;
+    }
+
+    document.getElementById("output").innerText = "You are now managing " + team + "!";
+}
+
